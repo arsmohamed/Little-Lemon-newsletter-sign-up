8e39046bc3345ce5e890cc7a56d34d5bccca9acb
Amr Mohamed <mero@Amrs-MacBook-Air.local>
2023-02-23 23:16:40 -0330

    finsihing the last task

--- screens/SubscribeScreen.js
+++ screens/SubscribeScreen.js
@@ -1,10 +1,12 @@
 import {React, useState} from 'react';
 import { View, Text, Image, Pressable,TextInput, StyleSheet, Alert } from 'react-native';
-import { ValidateEmail } from '../utils';
+import { ValidateEmail } from '../utils/index';
 
 const SubscribeScreen = () => {
+   
   const [value, onChangeText] = useState('');
   // Add subscribe screen code here
+  
   return <View style={styles.container}>
     <Image 
       style={styles.logo}
@@ -24,13 +26,16 @@ const SubscribeScreen = () => {
       value={value}
     />
     <Pressable
-      style={styles.button}
+    // value === '' ? '#A9A9A9' :
+      style={[styles.button, {backgroundColor: ValidateEmail(value) ? '#004225' :'#A9A9A9'}]}
+      disabled={!ValidateEmail(value)}
       onPress={ () => Alert.alert('Thanks for Subscribing, stay tuned!')}
     >
       <Text style={styles.buttonText}>Subscribe</Text>
     </Pressable>
   </View>;
 };
+
 
 export default SubscribeScreen;
 
@@ -60,8 +65,8 @@ const styles = StyleSheet.create({
     fontSize: 22,
     padding: 10,
     marginVertical: 10,
-    backgroundColor: '#004225',
-    borderColor: '#004225',
+    // backgroundColor: '#004225',
+    // borderColor: '#004225',
     borderWidth: 1,
     width: '90%',
     borderRadius: 20,
